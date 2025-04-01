19db8ea9d0e5d1abef3686f58e183e1a39383673
Mahsa Javadi <84246974+mahsa199170@users.noreply.github.com>
2025-04-01 12:28:53 -0400

    Update errorHandlerMiddleware.js

--- backend/middleware/errorHandlerMiddleware.js
+++ backend/middleware/errorHandlerMiddleware.js
@@ -1,35 +1,4 @@
-// ValidationError
-// CastError
-// const errorHandlingMiddleware = async (err, req, res, next) => {
-//   console.log('this is error', err.name);
 
-//   try {
-//     let error = { ...err };
-//     error.message = err.message;
-
-//     if (err.name === 'ValidatorError') {
-//       message = Object.values(err.errors).map((val) => val.message);
-//       error = new Error(message.join(', '));
-//       error.statusCode = 400;
-//     }
-
-//     if (err.name === 'CastError') {
-//       error = new Error('Resource not found!');
-//       error.statusCode = 404;
-//     }
-
-//     res.status(error.statusCode || 500).json({
-//       success: false,
-//       message: error.message || 'Server Error',
-//     });
-//     console.log(error);
-//   } catch (error) {
-//     console.log(error);
-//     next(error);
-//   }
-// };
-
-// module.exports = errorHandlingMiddleware;
 
 const errorHandlingMiddleware = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
